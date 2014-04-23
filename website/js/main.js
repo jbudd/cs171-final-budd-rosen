343c64a60e11fdeea418c0e8add93925e1014078
Jared Rosen <jaredarosen@gmail.com>
2014-04-23 01:27:58 -0400

    website skeleton

---
+++ website/js/main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() { });
+
+$(function() {
+	$("#modal").easyModal({
+		top: 100,
+		autoOpen: true,
+		overlayOpacity: 0.3,
+		overlayColor: "#333",
+		overlayClose: false,
+		closeOnEscape: false
+	});
+});
+
